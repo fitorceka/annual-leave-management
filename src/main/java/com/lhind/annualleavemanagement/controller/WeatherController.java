fbc3f18c56f23279b9e8c9dea8a415222ed2821d
Fitor <fitorceka@gmail.com>
2023-02-13 15:36:57 +0100

    improvement

--- src/main/java/com/lhind/annualleavemanagement/controller/WeatherController.java
+++ src/main/java/com/lhind/annualleavemanagement/controller/WeatherController.java
@@ -6,6 +6,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,13 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WeatherController {
 
+    @Value("${weather-key}")
+    private String key;
+
     @PostMapping("/weather")
     public String getWeatherData(@RequestParam("city") String city, Model model) throws IOException, InterruptedException {
 
         HttpRequest request = HttpRequest
             .newBuilder()
             .uri(URI.create("https://open-weather13.p.rapidapi.com/city/tirana"))
-            .header("X-RapidAPI-Key", "f4c15a6bffmsh0994b6a2f797943p154dadjsn97d654dc1fdd")
+            .header("X-RapidAPI-Key", key)
             .header("X-RapidAPI-Host", "open-weather13.p.rapidapi.com")
             .method("GET", HttpRequest.BodyPublishers.noBody())
             .build();
